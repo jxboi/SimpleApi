4c51be6993a497d6c6d12be3e32d1ddb893587c5
Jia Xing <jia_87@hotmail.com>
2020-06-26 12:21:38 +0800

    inital commit

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using SimpleApi.Controllers;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        
+        [Fact]
+        public void GetReturnsByName()
+        {
+            var returnValue = controller.Get();
+            var temp = ((SimpleApi.WeatherForecast[])returnValue)[0].TemperatureC;
+            Console.WriteLine($"value: {temp}");
+             
+            Assert.IsType(typeof(Int32), temp);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
