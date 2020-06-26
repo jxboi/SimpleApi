68be8234d581f6836945b4229fe444a6b673f97a
Jia Xing <jia_87@hotmail.com>
2020-06-26 12:48:15 +0800

    Remove HTTPS redirection

--- src/SimpleApi/Startup.cs
+++ src/SimpleApi/Startup.cs
@@ -36,7 +36,7 @@ namespace SimpleApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
